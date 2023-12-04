233796a104ac6e2f46c201c218aa7c9b107a1613
Santycadavid10 <78497375+Santycadavid10@users.noreply.github.com>
2023-12-04 18:06:40 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-var idioma = document.getElementById("idioma").value;
-console.log(typeof(idioma));
-if(idioma != "Open this select menu"){
-    console.log("hdhjdshdjhsklfjjjjjjjjjjjjjjjjjjj")
-}
-function hola(){
-    console.log("jsadkjfds");
+
+function idioma(){
+    var idioma = document.getElementById("idioma").value;
+    console.log(idioma);
+    if(idioma == "ingles"){
+        var titulohamburgesa1 = "burger with salad";
+        var descripcionhamburgesa1 = "Enjoy a delicious delicious burger with chicken nugets"
+        document.getElementById("titulohamburgesa1").innerText = titulohamburgesa1;
+        document.getElementById("descripcionhamburgesa1").innerText = descripcionhamburgesa1;
+
+    }
 }
