ae2edc500839f3e04a8fc95f34795004a404ea9e
Santycadavid10 <78497375+Santycadavid10@users.noreply.github.com>
2023-12-04 17:20:42 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+var idioma = document.getElementById("idioma").value;
+console.log(typeof(idioma));
+if(idioma != "Open this select menu"){
+    console.log("hdhjdshdjhsklfjjjjjjjjjjjjjjjjjjj")
+}
+function hola(){
+    console.log("jsadkjfds");
+}
